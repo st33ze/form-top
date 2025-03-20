9ef3f8f1245d2d5a89396ed2f21c692fb7590f76
st33ze <paweltrojnara@gmail.com>
2025-03-20 14:17:03 +0100

    feat(form): helper function for creating dom elements

--- form/form.js
+++ form/form.js
@@ -1,3 +1,9 @@
+function createNode(tag, attributes={}) {
+  const element = document.createElement(tag);
+  Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
+  return element;
+}
+
 class Form {
   static create() {
     const  testElement  = document.createElement('div');
