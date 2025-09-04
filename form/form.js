9bc605cb053c28057e2932c6bedf00afec21184a
st33ze <paweltrojnara@gmail.com>
2025-09-04 13:23:09 +0200

    feat(form): Input class

--- form/form.js
+++ form/form.js
@@ -1,5 +1,31 @@
 import COUNTRIES from './countries/countries.js';
 
+class Input {
+  static create({ type, id, labelName }) {
+    const container = document.createElement('div');
+    container.className = 'form-element';
+
+    const input = document.createElement('input');
+    input.type = type;
+    input.id = id || type;
+    input.placeholder = ' ';
+    input.autocorrect = 'off';
+    input.autocapitalize = 'none';
+    input.spellcheck = 'false';
+
+    const label = document.createElement('label');
+    label.for = id;
+    label.textContent = labelName || Input.capitalizeFirst(type);
+
+    container.append(input, label);
+    return container;
+  }
+
+  static capitalizeFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+}
+
 class Form {
   static create() {
     const form = document.createElement('form');
