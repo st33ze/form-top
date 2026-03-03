553ab2ee537911a6f988d1ba162719d54be7cbdd
st33ze <paweltrojnara@gmail.com>
2026-03-03 15:31:40 +0100

    feat: input wrapper for passwords

---
+++ form/PasswordInput.js
@@ -0,0 +1,41 @@
+import icons from "./icons.js";
+import Input from "./Input.js";
+import { createIconButton } from "./utility.js";
+
+export default class PasswordInput {
+
+  #field;
+  #button;
+
+  constructor({ id, labelName }) {
+    this.#field = new Input({
+      type: 'password',
+      id,
+      labelName,
+      attrs: { required: '', autocomplete: 'new-password' }
+    });
+
+    this.#field.input.classList.add('password-input');
+
+    this.#button = createIconButton(icons.show, 'Show password');
+    this.#button.classList.add('password-toggle');
+
+    this.#button.addEventListener('click', () => this.#toggle());
+
+    this.#field.node.appendChild(this.#button);
+  }
+
+  #toggle() {
+    const input = this.#field.input;
+    const isPassword = input.type === 'password';
+
+    input.type = isPassword ? 'text': 'password';
+    this.#button.setAttribute('aria-label', isPassword ? 'Hide password': 'Show password');
+    this.#button.innerHTML = isPassword ? icons.hide: icons.show;
+  }
+
+  get node() {
+    return this.#field.node;
+   }
+
+}
