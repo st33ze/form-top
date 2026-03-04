caf835d59ff312f0a1dbaa7d287ee3dab55eee70
st33ze <paweltrojnara@gmail.com>
2026-03-04 12:16:36 +0100

    refactor(CountrySelect): specific country select component that extends Select component

--- form/CountrySelect.js
+++ form/CountrySelect.js
@@ -1,186 +1,37 @@
 import COUNTRIES from "./countries/countries.js";
-import icons from "./icons.js";
-import Input from "./Input.js";
-import { createIconButton } from "./utility.js";
-
+import Select from "./Select.js";
 
 export default class CountrySelect {
-
-  #container
-  #refs
-  #options
-  #selectedValue
+  #select
 
   constructor() {
-    const { container, refs } = CountrySelect.#create();
-    this.#container = container;
-    this.#refs = refs;
-    this.#options = CountrySelect.#createOptionsMap();
-    
-    CountrySelect.#fillDropdown(this.#refs.dropdown, this.#options.values());
-    
-    this.#addEventListeners();
-  }
-
-  static #create() {
-    const container = Input.create({
-      type: 'text',
-      id: 'country-input',
-      labelName: 'Country'
+    this.#select = new Select({
+      id: 'country',
+      name: 'country',
+      labelName: 'Country',
+      attrs: { required: '', autocomplete: 'country'},
+      renderOptions: CountrySelect.#render
     });
 
-    const closeBtn = createIconButton(icons.close, 'Close country selection');
-    closeBtn.classList.add('close-modal-btn');
-
-    const label = container.querySelector('label');
-    label.id = 'country-label';
-
-    const selectedDisplay = document.createElement('button');
-    selectedDisplay.className = 'selected-display';
-    selectedDisplay.type = 'button';
-    selectedDisplay.setAttribute('aria-haspopup', 'listbox');
-    selectedDisplay.setAttribute('aria-expanded', 'false');
-    selectedDisplay.setAttribute('aria-labelledby', label.id);
-    
-
-    const input = container.querySelector('input');
-    input.className = 'country-input';
-    input.role = 'combobox';
-    input.setAttribute('autocomplete', 'new-password');
-    input.setAttribute('aria-expanded', 'false');
-    input.setAttribute('aria-autocomplete', 'list');
-    input.setAttribute('aria-controls', 'country-dropdown');
-
-    const dropdown = document.createElement('div');
-    dropdown.id = 'country-dropdown';
-    dropdown.className = 'dropdown';
-    dropdown.role = 'listbox';
-
-    container.prepend(closeBtn, selectedDisplay);
-    container.appendChild(dropdown);
-
-    return {
-      container,
-      refs: {closeBtn, selectedDisplay, input, dropdown}
-    };
+    this.#select.populate(COUNTRIES.map(c => ({value: c.code, label: c.name})));
   }
 
-  static #createCountryElement(country) {
-    const node = document.createElement('div');
-    node.className = 'dropdown-item';
-    node.setAttribute('role', 'option');
-    node.setAttribute('aria-selected', 'false');
-    node.setAttribute('tabindex', '-1');
-    node.dataset.countryCode = country.code;
-
+  static #render(li, {value, label}) {
     const img = document.createElement('img');
-    img.className = 'flag';
-    img.src = `form/countries/flags/${country.code}.svg`;
-    img.alt = `${country.name} flag`;
+    img.className = 'country-flag';
+    img.src = `/form/countries/flags/${value}.svg`;
+    img.alt = '';
+    img.loading = 'lazy';
+    img.setAttribute('aria-hidden', 'true');
 
     const span = document.createElement('span');
     span.className = 'country-name';
-    span.textContent = country.name;
+    span.textContent = label;
 
-    node.append(img, span);
-    return node;
+    li.append(img, span);
   }
 
-  static #createOptionsMap() {
-    const options = new Map();
-    COUNTRIES.forEach(country => {
-      const element = CountrySelect.#createCountryElement(country);
-      options.set(country.code, { ...country, element });
-    });
-    return options;
+  get node() {
+    return this.#select.node;
   }
-
-  static #fillDropdown(dropdown, options) {
-    for (const option of options) {
-      dropdown.appendChild(option.element);
-    }
-  }
-  
-  static #filterOptions(query, options) {
-    for (const option of options) {
-      const includesQuery = option.name.toLowerCase().includes(query);
-      option.element.style.display = includesQuery ? '' : 'none';
-    }
-  }
-
-  #getSelectedElement() {
-    return this.#options.get(this.#selectedValue)?.element;
-  }
-
-  #updateSelectedDisplay() {
-    const copy = this.#getSelectedElement().cloneNode(true);
-    this.#refs.selectedDisplay.replaceChildren(...copy.children);
-  }
-
-  #select(option) {
-    const selected = this.#getSelectedElement();
-    if (option !== selected) {
-      selected?.setAttribute('aria-selected', 'false');
-      this.#selectedValue = option.dataset.countryCode;
-      option.setAttribute('aria-selected', 'true');
-
-      this.#updateSelectedDisplay();
-    }
-  }
-
-  #addEventListeners() {
-    const {input, selectedDisplay, dropdown, closeBtn} = this.#refs;
-    
-    const showDropdown = () => {
-      input.setAttribute('aria-expanded', 'true');
-      selectedDisplay.setAttribute('aria-expanded', 'true');
-
-      const selected = this.#getSelectedElement();
-      if (selected) {
-        selected.scrollIntoView({ block:'center', behavior: 'instant'});
-        selected.setAttribute('tabindex', '0');
-      }
-
-      input.focus();
-    };
-
-    const hideDropdown = () => {
-      input.setAttribute('aria-expanded', 'false');
-      selectedDisplay.setAttribute('aria-expanded', 'false');
-
-      const focusTarget = this.#getSelectedElement() ? selectedDisplay: input;
-      focusTarget.focus();
-    };
-    
-    const isExpanded = () => input.getAttribute('aria-expanded') === 'true';
-
-    input.addEventListener('input', () => {
-      if (!isExpanded()) showDropdown();
-    });
-    selectedDisplay.addEventListener('click', showDropdown);
-
-    closeBtn.addEventListener('click', hideDropdown);
-    this.#container.addEventListener('keydown', e => {
-      if (e.key === 'Escape' && isExpanded()) hideDropdown();
-    });
-
-    dropdown.addEventListener('click', e => {
-      const option = e.target.closest('.dropdown-item');
-      if (option) {
-        this.#select(option);
-        hideDropdown();
-      }
-    });
-    
-    input.addEventListener('input', () => {
-      const term = input.value.toLowerCase().trim();
-      CountrySelect.#filterOptions(term, this.#options.values());
-    });
-
-  }
-
-  get element() {
-    return this.#container;
-  }
-
 }
