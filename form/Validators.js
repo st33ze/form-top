4d3d50883e22605d36dbe1e7a764379d58b99f6c
st33ze <paweltrojnara@gmail.com>
2026-03-06 12:25:51 +0100

    feat(Validators): minLength validator

--- form/Validators.js
+++ form/Validators.js
@@ -4,5 +4,8 @@ export const required = () => ({ value }) =>
 export const noEdgeSpaces = () => ({ value }) =>
   value.trim() === value ? '': 'Field can\'t start or end with spaces';
 
+export const minLength = (length) => ({ value }) => 
+  value.length >= length ? '': `Field has to be at least ${length} characters`;
+
 export const email = () => ({ value }) => 
   /\S+@\S+\.\S+/.test(value) ? '' : 'Invalid email';
