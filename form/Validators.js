8f8e58782ee5355f007702bd4d46485cdeb8b784
st33ze <paweltrojnara@gmail.com>
2026-03-06 15:03:09 +0100

    feat(Validators): valueMatch validator

--- form/Validators.js
+++ form/Validators.js
@@ -2,10 +2,17 @@ export const required = () => ({ value }) =>
   value.trim() ? '' : 'Field required';
 
 export const noEdgeSpaces = () => ({ value }) =>
-  value.trim() === value ? '': 'Field can\'t start or end with spaces';
+  value.trim() === value ? '' : 'Field can\'t start or end with spaces';
 
 export const minLength = (length) => ({ value }) => 
-  value.length >= length ? '': `Field has to be at least ${length} characters`;
+  value.length >= length ? '' : `Field has to be at least ${length} characters`;
+
+export const valueMatch = (sourceField, error) => 
+  ({ value }) => {
+    if (!value) return '';
+
+    return sourceField.value === value ? '' : error;
+  }
 
 export const email = () => ({ value }) => 
   /\S+@\S+\.\S+/.test(value) ? '' : 'Invalid email';
